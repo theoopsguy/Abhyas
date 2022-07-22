7d02322001e584b593388268c7dd6ea96e6086fe
tanishchugh01 <tanishchugh01@gmail.com>
2022-07-23 03:44:19 +0530

    Values were not changing on submit added onchange method on every input

--- frontend/src/components/Contribute/Contribute.jsx
+++ frontend/src/components/Contribute/Contribute.jsx
@@ -10,7 +10,7 @@ import ShowDropDowns from "../ShowDropDowns/ShowDropDowns";
 
 function Contribute() {
   const [subject, setSubject] = useState("");
-  const [unit, setUnit] = useState("");
+  const [unit, setUnit] = useState("6229fb510b251f2757d2b4ee");
   return (
     <div>
       <NavigationBar />
