3e6db871f7878e227d650224b4ddd51236320a09
theoopsguy <theoopsguy01@gmail.com>
2022-07-27 22:41:05 +0530

    Reverting changes as issue not caused by Card.Img

--- client/src/components/Landing/LandingCard.jsx
+++ client/src/components/Landing/LandingCard.jsx
@@ -5,8 +5,7 @@ function LandingCard({ imgSource = "", title = "", link = "/" }) {
   return (
     <Link to={link} style={{ textDecoration: "none" }}>
       <Card style={{ width: "20rem" }}>
-        {/* <Card.Img style={{ height: "15rem" }} variant="top" src={imgSource}></Card.Img> */}
-        <img src={imgSource} className="LandingCardImg" />
+        <Card.Img style={{ height: "15rem" }} variant="top" src={imgSource}></Card.Img>
         <Card.Body style={{fontSize:"23px"}} className="text-center"><b>{title}</b></Card.Body>
       </Card>
     </Link>
