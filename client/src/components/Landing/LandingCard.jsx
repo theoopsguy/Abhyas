e1670b6975c92ae7169da610fa97749a7f647206
theoopsguy <theoopsguy01@gmail.com>
2022-07-27 22:25:44 +0530

    Try image with html img tag instead of Card.Img

--- client/src/components/Landing/LandingCard.jsx
+++ client/src/components/Landing/LandingCard.jsx
@@ -5,7 +5,8 @@ function LandingCard({ imgSource = "", title = "", link = "/" }) {
   return (
     <Link to={link} style={{ textDecoration: "none" }}>
       <Card style={{ width: "20rem" }}>
-        <Card.Img style={{ height: "15rem" }} variant="top" src={imgSource}></Card.Img>
+        {/* <Card.Img style={{ height: "15rem" }} variant="top" src={imgSource}></Card.Img> */}
+        <img src={imgSource} className="LandingCardImg" />
         <Card.Body style={{fontSize:"23px"}} className="text-center"><b>{title}</b></Card.Body>
       </Card>
     </Link>
